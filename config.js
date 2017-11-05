2cacf4002e471155319ebb57801a44f56330f04f
Hayat Mazzarella <imhighyat@gmail.com>
2017-11-05 01:16:41 -0700

    added runServer function

---
+++ config.js
@@ -0,0 +1,6 @@
+//configure values for the Mongo database URL and the port the application will run on
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                      'mongodb://localhost/restaurants-app'; //edit for the name of the db
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
 
+//let us use ES6 promises instead of Mongoose's built in
+mongoose.Promise = global.Promise;
+
+//we require config.js which has the port and db values
+const {PORT, DATABASE_URL} = require('./config');
+const {Blogposts} = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+//will store the server object we are starting
+let server;
+
+//connects to the db and starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT){
+	return new Promise ((resolve, reject) => {
+		//connect to the db first
+		mongoose.connect(databaseUrl, err => {
+			//if there is an error connecting, return the promise
+			//with reject and the error
+			if(err){
+				return reject(err);
+			}
+			//if not, move on to starting a server
+			server = app.listen(port, () => {
+				console.log(`The app is listening on ${port}.`);
+				resolve();
+			})
+			//if there is an error, disconnect server and return a reject
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
